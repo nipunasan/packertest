030992e6cda6e8d6566a60f172e477b4814cff33
Nipuna Perera <nipunasan@gmail.com>
2022-07-17 17:31:12 +0530

    build break to two

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,21 @@
 pipeline {
     agent any
     stages {
-        stage('Packer - Build') {
+        stage('Packer - Build init') {
             steps{
                 sh """
                 #!/bin/bash
                 cd ./packer/
                 packer init .
+                packer build -force .
+                """
+            }
+        }
+        stage('Packer - Build') {
+            steps{
+                sh """
+                #!/bin/bash
+                cd ./packer/
                 packer build -force .
                 """
             }
