70f61f7bfa862030ae4c12cd888b36d6676f3e6a
Nipuna Perera <nipunasan@gmail.com>
2022-07-17 18:01:08 +0530

    jenkins file change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,25 +1,19 @@
 pipeline {
     agent any
     stages {
+        environment {
+            PACKER_LOG=1
+        }
         stage('Packer - Build init') {
             steps{
-                sh """
-                #!/bin/bash
-                export PACKER_LOG=1
-                cd ./packer/
-                packer init .
-                packer build -force .
-                """
+                sh 'cd ./packer/'
+                sh 'packer init .'
             }
         }
         stage('Packer - Build') {
             steps{
-                sh """
-                #!/bin/bash
-                export PACKER_LOG=1
-                cd ./packer/
-                packer build -force .
-                """
+                sh 'cd ./packer/'
+                sh 'packer build -force .'
             }
         }
     }
